e31871db6dd0dbe5f9d6ebeb534421df310ae383
Omar <o.bahutair@gmail.com>
2022-03-12 19:43:49 +0400

    add the input function

--- main.cpp
+++ main.cpp
@@ -6,14 +6,33 @@ using namespace std;
 struct position
 {
     int x, y;
+    bool operator<(int num)
+    {
+        if ((*this).x < num || (*this).y < num)
+            return true;
+        else
+            return false;
+    }
+
+    bool operator>(int num)
+    {
+        if ((*this).x > num || (*this).y > num)
+            return true;
+        else
+            return false;
+    }
 };
 
 void print_board(char arr[3][3]);
+position input(int player);
 
 int main()
 {
     char board[3][3] = {};
     print_board(board);
+    position move = input(1);
+
+    return 0;
 }
 
 /*
@@ -39,4 +58,21 @@ void print_board(char arr[3][3])
             cout << "  -----|-----|-----" << endl;
         }
     }
+}
+
+position input(int player)
+{
+    position move;
+    char pos_x, pos_y;
+    do
+    {
+        cout << "Player " << player << "(x then y): ";
+        cin >> pos_x >> pos_y;
+
+        move.x = pos_x - 48;
+        move.y = pos_y - 48;
+
+    } while (move > 4 || move < 0);
+
+    return move;
 }
